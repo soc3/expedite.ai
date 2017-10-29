608d7e714c7b0cdc8c1b2aa01a133308da9f4419
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-10-29 09:11:46 +0530

    add static files

--- helpdesk/apps.py
+++ helpdesk/apps.py
@@ -1,8 +1,4 @@
-f{
-    'open': 1,
-    'closed': 2,
-    'spam': 3
-}rom django.apps import AppConfig
+from django.apps import AppConfig
 
 
 class HelpdeskConfig(AppConfig):
--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.conf import settings
 
 from helpdesk.views import get_issues
@@ -7,3 +8,6 @@ from helpdesk.views import get_issues
 urlpatterns = [
     url(r'^issues/', get_issues)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL)
